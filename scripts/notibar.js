f0978a2965be4f70461ad41c17d6f6e9310dc3dc
charlie-jay2 <109756523+charlie-jay2@users.noreply.github.com>
2024-08-01 21:02:08 +0100

    Added CE Announcement

--- scripts/notibar.js
+++ scripts/notibar.js
@@ -48,3 +48,24 @@ if (notibar.enabled) {
     var navBar = document.querySelector('header nav');
     navBar.parentNode.insertBefore(notificationBar, navBar.nextSibling);
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('transcript-modal');
+    const btn = document.getElementById('transcript-btn');
+    const span = document.getElementsByClassName('close-btn')[0];
+
+    btn.onclick = function () {
+        modal.style.display = 'block';
+    }
+
+    span.onclick = function () {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
