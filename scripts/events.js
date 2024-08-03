c1c48b88968cc2460b550026fd1717dcae055b51
charlie-jay2 <109756523+charlie-jay2@users.noreply.github.com>
2024-08-03 15:08:10 +0100

    Add files via upload

--- scripts/events.js
+++ scripts/events.js
@@ -3,12 +3,12 @@ let doorsClosed = false;
 let noEvent = false;
 
 function handleEventSoon() {
-    const aopening = document.querySelector('.aopening');
-    aopening.style.marginLeft = '50px';
+  const aopening = document.querySelector('.aopening');
+  aopening.style.marginLeft = '50px';
 
-    const eventInfo = document.createElement('section');
-    eventInfo.classList.add('einfo');
-    eventInfo.innerHTML = `
+  const eventInfo = document.createElement('section');
+  eventInfo.classList.add('einfo');
+  eventInfo.innerHTML = `
     <h2>The event at Amplify is starting soon.</h2>
     <p>Join the game here: 
       <a href="https://www.roblox.com/games/16919071746/finlay550s-Place-Number-545" target="_blank">ENTER</a><br />
@@ -18,62 +18,62 @@ function handleEventSoon() {
     <p>The event will open at 12pm and officially start at 12:15pm.</p>
   `;
 
-    document.body.appendChild(eventInfo);
+  document.body.appendChild(eventInfo);
 
-    const einfoStyle = document.createElement('style');
-    einfoStyle.innerHTML = `
+  const einfoStyle = document.createElement('style');
+  einfoStyle.innerHTML = `
     .einfo {
       text-align: center;
-      margin-top: -500px;
+      margin-top: -800px;
       margin-left: 500px;
       font-family: "aileron-heavy";
     }
   `;
-    document.head.appendChild(einfoStyle);
+  document.head.appendChild(einfoStyle);
 }
 
 function handleDoorsClosed() {
-    const aopening = document.querySelector('.aopening');
-    aopening.style.marginLeft = '50px';
+  const aopening = document.querySelector('.aopening');
+  aopening.style.marginLeft = '50px';
 
-    const eventInfo = document.createElement('section');
-    eventInfo.classList.add('einfo');
-    eventInfo.innerHTML = `
+  const eventInfo = document.createElement('section');
+  eventInfo.classList.add('einfo');
+  eventInfo.innerHTML = `
     <h2>The event has now started and the doors are now closed.</h2>
     <p>Thanks for attending.</p>
   `;
 
-    document.body.appendChild(eventInfo);
+  document.body.appendChild(eventInfo);
 
-    const einfoStyle = document.createElement('style');
-    einfoStyle.innerHTML = `
+  const einfoStyle = document.createElement('style');
+  einfoStyle.innerHTML = `
     .einfo {
       text-align: center;
-      margin-top: -500px;
+      margin-top: -700px;
       margin-left: 500px;
       font-family: "aileron-heavy";
     }
   `;
-    document.head.appendChild(einfoStyle);
+  document.head.appendChild(einfoStyle);
 }
 
 function handleNoEvent() {
-    const aopeningImages = document.querySelectorAll('.aopening');
-    aopeningImages.forEach(image => {
-        image.style.display = 'none';
-    });
+  const aopeningImages = document.querySelectorAll('.aopening');
+  aopeningImages.forEach(image => {
+    image.style.display = 'none';
+  });
 
-    const noEventMessage = document.createElement('div');
-    noEventMessage.classList.add('no-event-message');
-    noEventMessage.innerHTML = `
+  const noEventMessage = document.createElement('div');
+  noEventMessage.classList.add('no-event-message');
+  noEventMessage.innerHTML = `
     <h2>There is currently no upcoming events, please check back later.</h2>
   `;
 
-    const footer = document.querySelector('.footer');
-    document.body.insertBefore(noEventMessage, footer);
+  const footer = document.querySelector('.footer');
+  document.body.insertBefore(noEventMessage, footer);
 
-    const noEventStyle = document.createElement('style');
-    noEventStyle.innerHTML = `
+  const noEventStyle = document.createElement('style');
+  noEventStyle.innerHTML = `
     .no-event-message {
       text-align: center;
       margin-top: -50px;
@@ -84,13 +84,13 @@ function handleNoEvent() {
       margin-top: 450px;
     }
   `;
-    document.head.appendChild(noEventStyle);
+  document.head.appendChild(noEventStyle);
 }
 
 if (noEvent) {
-    handleNoEvent();
+  handleNoEvent();
 } else if (doorsClosed) {
-    handleDoorsClosed();
+  handleDoorsClosed();
 } else if (eventSoon) {
-    handleEventSoon();
+  handleEventSoon();
 }
--- scripts/notibar.js
+++ scripts/notibar.js
@@ -2,46 +2,49 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch('notibar-config.json')
         .then(response => response.json())
         .then(notibar => {
-            if (notibar.enabled) {
-                var notificationBar = document.createElement('div');
-                notificationBar.style.backgroundColor = notibar.backgroundColor;
-                notificationBar.style.color = notibar.textColor;
-                notificationBar.style.textAlign = 'center';
-                notificationBar.style.padding = '10px';
-                notificationBar.style.height = notibar.height;
-                notificationBar.style.lineHeight = notibar.height;
-                notificationBar.style.fontSize = notibar.fontSize;
-                notificationBar.style.width = '100%';
-                notificationBar.style.boxSizing = 'border-box';
-
-                notificationBar.innerHTML = notibar.text;
-
-                var heroElement = document.querySelector('.hero');
-                var closeButtonEnabled = notibar.closeButton;
-                var heroMarginTop = closeButtonEnabled ? '90px' : '110px';
-
-                if (closeButtonEnabled) {
-                    var closeButton = document.createElement('span');
-                    closeButton.innerHTML = '&times;';
-                    closeButton.style.marginLeft = '20px';
-                    closeButton.style.cursor = 'pointer';
-                    closeButton.style.fontSize = '20px';
-                    closeButton.onclick = function () {
-                        notificationBar.style.display = 'none';
-                        heroElement.style.marginTop = '110px'; // Adjust the margin-top when hidden
-                    };
-                    notificationBar.appendChild(closeButton);
-                } else {
-                    heroElement.style.marginTop = heroMarginTop; // Set initial margin-top
-                }
-
-                var navBar = document.querySelector('header nav');
-                navBar.parentNode.insertBefore(notificationBar, navBar.nextSibling);
-
-                if (closeButtonEnabled) {
-                    heroElement.style.marginTop = heroMarginTop; // Set initial margin-top if close button is enabled
-                }
-            }
+            createNotificationBar(notibar);
         })
-        .catch(error => console.error('Error fetching notibar configuration:', error));
+
+    function createNotificationBar(notibar) {
+        if (notibar.enabled) {
+            var notificationBar = document.createElement('div');
+            notificationBar.style.backgroundColor = notibar.backgroundColor;
+            notificationBar.style.color = notibar.textColor;
+            notificationBar.style.textAlign = 'center';
+            notificationBar.style.padding = '10px';
+            notificationBar.style.height = notibar.height;
+            notificationBar.style.lineHeight = notibar.height;
+            notificationBar.style.fontSize = notibar.fontSize;
+            notificationBar.style.width = '100%';
+            notificationBar.style.boxSizing = 'border-box';
+
+            notificationBar.innerHTML = notibar.text;
+
+            var heroElement = document.querySelector('.hero');
+            var closeButtonEnabled = notibar.closeButton;
+            var heroMarginTop = closeButtonEnabled ? '90px' : '110px';
+
+            if (closeButtonEnabled) {
+                var closeButton = document.createElement('span');
+                closeButton.innerHTML = '&times;';
+                closeButton.style.marginLeft = '20px';
+                closeButton.style.cursor = 'pointer';
+                closeButton.style.fontSize = '20px';
+                closeButton.onclick = function () {
+                    notificationBar.style.display = 'none';
+                    heroElement.style.marginTop = '110px'; // Adjust the margin-top when hidden
+                };
+                notificationBar.appendChild(closeButton);
+            } else {
+                heroElement.style.marginTop = heroMarginTop; // Set initial margin-top
+            }
+
+            var navBar = document.querySelector('header nav');
+            navBar.parentNode.insertBefore(notificationBar, navBar.nextSibling);
+
+            if (closeButtonEnabled) {
+                heroElement.style.marginTop = heroMarginTop; // Set initial margin-top if close button is enabled
+            }
+        }
+    }
 });
